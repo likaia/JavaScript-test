a5dc9c41f5c5bd9812b3b69fbb6d0ba8b5b2eed0
MagicalProgrammer <645289137@qq.com>
2020-05-22 14:55:05 +0800

    feat: 实现十进制转任意进制函数

--- StackTest/Examples.js
+++ StackTest/Examples.js
@@ -54,16 +54,49 @@ const decimalToBinaryObjStack = function (decNumber) {
     return binaryString;
 }
 
+// 进制转换
+const hexConversion = function (decNumber,hex) {
+    const stack = new ObjStack();
+    const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // 结果进行除法运算后的值
+    let number = decNumber;
+    // 余数
+    let rem;
+    // 转换后的进制字符串
+    let hexString = "";
 
-const testNumber = 999989993232287;
-console.time("数组栈");
-console.log(decimalToBinary(testNumber));
-console.timeEnd("数组栈");
+    // 判断进制是否有效
+    if(!(hex >= 2 && hex <= 36)){
+        return "";
+    }
 
-console.time("对象栈");
-const testObjNumber = 999989993232287;
-console.log(decimalToBinaryObjStack(testObjNumber));
-console.timeEnd("对象栈");
+    while (number > 0){
+        // 取余数
+        rem = Math.floor(number % hex);
+        // 入栈
+        stack.push(rem);
+        // 对结果进行除法运算
+        number = Math.floor(number / hex);
+    }
+    while (!stack.isEmpty()){
+        console.log("栈内元素:",stack.peek())
+        console.log(`栈内元素(${hex}进制):`,digits[stack.peek()]);
+        // 拼接进制字符
+        hexString += digits[stack.pop()];
+    }
+    return  hexString;
+}
 
+// const testNumber = 999989993232287;
+// console.time("数组栈");
+// console.log(decimalToBinary(testNumber));
+// console.timeEnd("数组栈");
+//
+// console.time("对象栈");
+// const testObjNumber = 999989993232287;
+// console.log(decimalToBinaryObjStack(testObjNumber));
+// console.timeEnd("对象栈");
 
+const number = 989098;
+hexConversion(number,16)
 
