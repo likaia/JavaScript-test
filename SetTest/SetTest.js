aa1535c04c891d806d1ef4a2d8efbe6a46036ff9
likai <1195419506@qq.com>
2020-06-03 23:04:00 +0800

    feat: 实现集合的其他函数
    实现交集运算函数
    实现差集运算函数
    实现子集运算函数

--- SetTest/SetTest.js
+++ SetTest/SetTest.js
@@ -6,17 +6,20 @@ set.add(12);
 set.add(13);
 set.delete(11);
 console.log(set.size())
-console.log(set.values());
+console.log("集合中的元素",set.values());
 
 const A = new Set();
 A.add(10);
 A.add(11);
 A.add(12);
 A.add(13);
-
+A.add(1);
+A.add(2);
 const B = new Set();
 B.add(1);
 B.add(2);
 B.add(3);
 // 求A和B的并集
-console.log(A.union(B));
+console.log("A和B的并集",A.union(B).values());
+// 求A和B的交集
+console.log("A和B的交集",A.intersection(B).values());
--- SetTest/lib/Set.ts
+++ SetTest/lib/Set.ts
@@ -61,10 +61,69 @@ export default class Set<T>{
 
     // 并集
     union(otherSet: Set<T>){
+        // 声明交集变量
         const unionSet = new Set();
         this.values().forEach(value => unionSet.add(value));
         otherSet.values().forEach(value => unionSet.add(value));
         return unionSet;
     }
 
+    // 交集
+    intersection(otherSet: Set<T>) {
+        // 声明并集变量
+        const intersectionSet = new Set();
+        // 获取当前实例集合中的元素
+        const values  = this.values();
+        // 获取另一个集合中的元素
+        const otherValues = otherSet.values();
+        // 假设当前实例集合中的元素最多
+        let biggerSet = values;
+        // 假设另一个元素集合中的元素最少
+        let smallerSet = otherValues;
+        // 如果另一个集合中的元素个数比当前元素集合中的个数多，则交换变量
+        if(otherValues.length - values.length > 0){
+            biggerSet = otherValues;
+            smallerSet = values;
+        }
+        // 遍历元素最少的集合数组，节约性能开销
+        smallerSet.forEach(value => {
+           if (biggerSet.includes(value)){
+               intersectionSet.add(value);
+           }
+        });
+        // 返回交集集合
+        return intersectionSet;
+    }
+
+    // 差集
+    difference(otherSet: Set<T>) {
+        // 声明差集变量
+        const differenceSet = new Set();
+        // 遍历当前实例中的集合
+        this.values().forEach(value => {
+            // 如果当前遍历到元素不存在与另一个集合中，则将档当前元素添加进差集变量里
+           if(!otherSet.has(value)){
+               differenceSet.add(value);
+           }
+        });
+        // 返回差集变量
+        return differenceSet;
+    }
+
+    // 子集
+    isSubsetOf(otherSet: Set<T>) {
+        if(this.size() > otherSet.size()){
+            return false;
+        }
+
+        let isSubset = true;
+        this.values().every(value => {
+            if(otherSet.has(value)){
+                isSubset = false;
+                return false;
+            }
+            return true;
+        });
+        return isSubset;
+    }
 }
