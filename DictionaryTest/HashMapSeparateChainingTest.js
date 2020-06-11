1f1c700a587c4410d06d7dfdd30c206d43650396
likai <1195419506@qq.com>
2020-06-11 22:08:39 +0800

    feat: 分析分离链接解决哈希冲突问题

--- DictionaryTest/HashMapSeparateChainingTest.js
+++ DictionaryTest/HashMapSeparateChainingTest.js
@@ -2,4 +2,7 @@ import HashMapSeparateChaining from "./lib/HashMapSeparateChaining.ts";
 
 const hashMapSC = new HashMapSeparateChaining();
 hashMapSC.put("name","张三");
+hashMapSC.put("id",11);
+hashMapSC.remove("id");
 console.log(hashMapSC.get("name"));
+console.log(hashMapSC.toString());
--- DictionaryTest/lib/HashMapSeparateChaining.ts
+++ DictionaryTest/lib/HashMapSeparateChaining.ts
@@ -26,13 +26,17 @@ export default class HashMapSeparateChaining<K,V> extends HashMap<K, V> {
 
     // 获取链表中的值 
     get(key: K): V | undefined {
-
+        // 获取参数的hash值
         const position = this.hashCode(key);
+        // 获取目标元素位置存储的链表结构元素
         const linkedList = this.tableLink[position];
         if (linkedList !=null && !linkedList.isEmpty()){
+            // 获取链表头部数据
             let current = linkedList.getHead();
             while (current != null){
+                // 遍历链表，找到链表中与目标参数相同的数据
                 if (current.element.key === key){
+                    // 返回目标key对应的value值
                     return current.element.value;
                 }
                 current = current.next;
@@ -43,13 +47,18 @@ export default class HashMapSeparateChaining<K,V> extends HashMap<K, V> {
 
     remove(key: K): boolean {
         const position = this.hashCode(key);
+        // 获取目标元素位置存储的链表结构元素
         const linkedList = this.tableLink[position];
         if (linkedList != null && !linkedList.isEmpty()){
+            // 获取链表头部元素
             let current = linkedList.getHead();
             while (current != null){
+                // 遍历链表，找到与目标元素相同的数据
                 if (current.element.key === key){
+                    // 将当前链表中的元素从链表中移除
                     linkedList.remove(current.element);
                     if (linkedList.isEmpty()){
+                        // 链表为空，删除目标位置元素
                         delete this.tableLink[position];
                     }
                     return true;
@@ -61,11 +70,7 @@ export default class HashMapSeparateChaining<K,V> extends HashMap<K, V> {
     }
 
     toString() {
-        if (this.isEmpty()) {
-            return '';
-        }
-        const keys = Object.keys(this.tableLink);
-        console.log(keys);
+        console.log(this.tableLink);
         return ``;
     }
 }
