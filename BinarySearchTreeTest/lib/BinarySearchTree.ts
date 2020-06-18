f2b83a08bb1da2088662a4ec9e6efdcf46e39c90
likai <1195419506@qq.com>
2020-06-19 00:04:21 +0800

    feat: 添加二叉搜索树相关文件

---
+++ BinarySearchTreeTest/lib/BinarySearchTree.ts
@@ -0,0 +1,15 @@
+/**
+ * 二叉搜索树的实现
+ */
+export default class BinarySearchTree<T> {
+    private key: T;
+
+    constructor(key: T) {
+        this.key = key;
+    }
+}
+
+// Node类用于表示二叉搜索树的每个节点
+export class Node {
+
+}
