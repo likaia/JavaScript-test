806db322c408dd87b8cd32ad018b3a028b2a4b95
likai <1195419506@qq.com>
2020-07-15 00:12:46 +0800

    feat: 添加堆的相关实现

---
+++ Heap/lib/MinHeap.ts
@@ -0,0 +1,31 @@
+import {Compare,ICompareFunction,defaultCompare} from "../../utils/Util.ts";
+
+/**
+ * 堆是一颗完全二叉树
+ *  1. 左子节点的位置: 2 * index + 1
+ *  2. 右子节点的位置: 2 * index + 2
+ *  3. 父节点的位置: index / 2
+ */
+export default class MinHeap<T>{
+    // 用数组来描述一个堆
+    protected heap: T[]
+
+    constructor(protected compareFn: ICompareFunction<T> = defaultCompare) {
+        this.heap = [];
+    }
+
+    // 获取左子节点的位置
+    getLeftIndex(index: number) {
+        return 2 * index +1;
+    }
+
+    // 获取右子节点的位置
+    getRightIndex(index: number) {
+        return 2 * index + 2;
+    }
+
+    // 获取父节点的位置
+    getParentIndex(index: number) {
+
+    }
+}
