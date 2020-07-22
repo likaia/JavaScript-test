9a0c48083cfba15743bbbd5a237170e6aeb424d4
likai <1195419506@qq.com>
2020-07-23 00:40:21 +0800

    fix: 完善图的实现方法
    修改tsconfig让其支持es6+的语法
    开启TS严格模式

--- src/Graph/lib/Graph.ts
+++ src/Graph/lib/Graph.ts
@@ -9,13 +9,53 @@ export default class Graph {
 
     // 添加顶点
     addVertex(v: string | number): void {
-        if (!this.vertices) {
+        // 顶点不存在于图中
+        if (!this.vertices.includes(v)) {
+            // 将该顶点添加到顶点列表中
             this.vertices.push(v);
+            // 在临接表中设置顶点v作为键，对应的字典值为一个空数组
             this.adjList.set(v, []);
         }
+    }
 
-        if (this.isDirected) {
-            console.log("aa");
+    // 添加线，连接顶点
+    addEdge(v: string | number, w: string | number): void {
+        // 添加顶点之前需要验证顶点v和w是否在图中，不存在就追加
+        if (!this.adjList.get(v)) {
+            this.addVertex(v);
         }
+        if (!this.adjList.get(w)) {
+            this.addVertex(w);
+        }
+
+        // 将w加入到v的临接表中，我们就得到了一条来自顶点v到顶点w的边
+        this.adjList.get(v)?.push(w);
+        if (!this.isDirected) {
+            // 如果是无向图则需要添加一条自w到v的边
+            this.adjList.get(w)?.push(v);
+        }
+    }
+
+    // 获取顶点列表
+    getVertices(): (string | number)[] {
+        return this.vertices;
+    }
+
+    // 获取临接表
+    getAdjList(): Dictionary<string | number, (string | number)[]> {
+        return this.adjList;
+    }
+
+    toString(): string {
+        let s = "";
+        for (let i = 0; i < this.vertices.length; i++) {
+            s += `${this.vertices[i]} -> `;
+            const neighbors = <Array<string | number>>this.adjList.get(this.vertices[i]);
+            for (let j = 0; j < neighbors.length; j++) {
+                s += `${neighbors[j]} `;
+            }
+            s += "\n";
+        }
+        return s;
     }
 }
