b26b7a56ff826328b0a37e6041e0e9f8ab643545
MagicalProgrammer <645289137@qq.com>
2020-08-20 18:32:33 +0800

    fix: 进一步完善最少硬币找零问题图解

--- src/algorithmDesignSkills/DesignSkillsTest.js
+++ src/algorithmDesignSkills/DesignSkillsTest.js
@@ -1,4 +1,4 @@
 import { DesignSkills } from "./lib/DesignSkills.ts";
 const designSkills = new DesignSkills();
-const result = designSkills.minCoinChange([1, 5, 10, 25], 13);
+const result = designSkills.minCoinChange([1, 5, 10, 25], 8);
 console.log(result);
