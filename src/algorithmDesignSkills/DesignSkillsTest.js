661aca416756104247670d1c4e9e7ee1e6c7510c
MagicalProgrammer <645289137@qq.com>
2020-08-24 18:07:18 +0800

    feat: 添加背包问题函数

--- src/algorithmDesignSkills/DesignSkillsTest.js
+++ src/algorithmDesignSkills/DesignSkillsTest.js
@@ -2,3 +2,9 @@ import { DesignSkills } from "./lib/DesignSkills.ts";
 const designSkills = new DesignSkills();
 const result = designSkills.minCoinChange([1, 5, 10, 25], 8);
 console.log(result);
+
+const values = [3, 4, 5],
+    weights = [2, 3, 4],
+    capacity = 5,
+    n = values.length;
+console.log(designSkills.knapSack(capacity, weights, values, n));
--- src/algorithmDesignSkills/lib/DesignSkills.ts
+++ src/algorithmDesignSkills/lib/DesignSkills.ts
@@ -1,6 +1,6 @@
 export class DesignSkills {
     /**
-     *
+     * 最少硬币找零
      * @param coins 硬币面额
      * @param amount 找零总金额
      */
@@ -37,5 +37,57 @@ export class DesignSkills {
             return (cache[amount] = min);
         };
         return makeChange(amount);
+    }
+
+    /**
+     * 背包问题
+     * 即: 给定一个固定大小、能够携重量w的背包，以及一组有价值和重量的物品，找出一个最佳解决方案，使得装入背包的物品的总重量不超过w,且总价值最大
+     * @param capacity 背包容量
+     * @param weights 物品的重量
+     * @param values 物品的价值
+     * @param n 物品数量
+     */
+    knapSack(capacity: number, weights: number[], values: number[], n: number): number {
+        // 声明并初始化需要寻找解决方案的矩阵
+        const kS: number[][] = [];
+        for (let i = 0; i <= n; i++) {
+            kS[i] = [];
+        }
+
+        for (let i = 0; i <= n; i++) {
+            for (let w = 0; w <= capacity; w++) {
+                if (i === 0 || w === 0) {
+                    // 忽略矩阵的第一列和第一行，只处理索引不为0的列和行
+                    kS[i][w] = 0;
+                } else if (weights[i - 1] <= w) {
+                    // 物品i的重量必须小于约束
+                    const a = values[i - 1] + kS[i - 1][w - weights[i - 1]];
+                    const b = kS[i - 1][w];
+                    // 当找到可以构成解决方案的物品时，选择价值最大的那个
+                    kS[i][w] = a > b ? a : b;
+                } else {
+                    // 总重量超出背包能够携带的重量，忽略它用之前的值
+                    kS[i][w] = kS[i - 1][w];
+                }
+            }
+        }
+
+        DesignSkills.findValues(n, capacity, kS, weights, values);
+        return kS[n][capacity];
+    }
+
+    private static findValues(n: number, capacity: number, kS: number[][], weights: number[], values: number[]): void {
+        let i = n;
+        let k = capacity;
+        console.log("gou cheng jie de wu pin ");
+        while (i > 0 && k > 0) {
+            if (kS[i][k] !== kS[i - 1][k]) {
+                console.log(`wu pin ${i} ke yi shi jie de yi bu fen w,v: ${weights[i - 1]}, ${values[i - 1]};`);
+                i--;
+                k -= kS[i][k];
+            } else {
+                i--;
+            }
+        }
     }
 }
