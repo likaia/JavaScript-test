8cbc144b00960e706db24bb4caccc60948073ebb
likai <1195419506@qq.com>
2020-09-25 00:00:50 +0800

    feat: 添加字符串替换空格解法

--- src/Example/ExampleTest.js
+++ src/Example/ExampleTest.js
@@ -1,7 +1,11 @@
 import { ArrayRepeatedNumber } from "./lib/ArrayRepeatedNumber.ts";
+import { StringOperate } from "./lib/StringOperate.ts";
 
 // 获取数组中任意一个重复数字
 const arrayRepeatedNumber = new ArrayRepeatedNumber([1, 5, 2, 3, 4, 5, 7]);
+// 字符串操作
+const stringOperate = new StringOperate();
+
 const result = arrayRepeatedNumber.findRepeated();
 if (result !== -1 && result != null) {
     console.log("数组中的任意一个重复数字为：" + result);
@@ -24,3 +28,11 @@ const result2 = arrayRepeatedNumber.findNumberWithTDM(
     12
 );
 console.log(result2);
+
+const str = "hello world, 空格测试";
+
+const result3 = stringOperate.replaceSpacesWithArray(str, "%20");
+const result4 = stringOperate.replaceSpacesWithString(str, "20%");
+
+console.log(result3);
+console.log(result4);
