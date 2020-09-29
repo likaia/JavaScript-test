7ecd5c2145b73906f6a1a84780a449af66b5f5be
likai <1195419506@qq.com>
2020-09-29 12:30:45 +0800

    feat: 修复重建二叉树问题注释

--- src/Example/lib/TreeOperate.ts
+++ src/Example/lib/TreeOperate.ts
@@ -18,22 +18,22 @@ export class TreeOperate<T> {
      * 中序遍历: 3 -> 6 -> 7 -> 8 -> 9 -> 13 -> 15
      *
      * 根据前序遍历的特点我们可知道下述信息：
-     *  1. 树的根结点的值是8
-     *  2. 将其代入中序遍历中，8位于中序遍历的第3号位置。
-     *  3. 根结点的左子树有3个元素，右子树有3个元素。
-     *  4. 它前面的元素为它的左子节点，它后面的元素为它的右子节点
-     * 在前序遍历中，他后面紧跟的3个元素就是它的左子树，剩余的就是它的右子树
+     *  1. 树根结点的值是8
+     *  2. 8位于中序遍历组合的第3号位置
+     *  3. 中序遍历组合中，8的左边是它的左子节点，剩余的就是8的右子节点。
+     *  4. 前序遍历组合中，8的右边3个元素是它的左子节点，剩余的元素是它的右子节点。
      *
      * @param prologueArr 前序遍历的结果
      * @param middleOrderArr 中序遍历的结果
      */
     buildBinaryTree(prologueArr: T[], middleOrderArr: T[]): Node<T> | null {
-        // 递归边界条件
+        // 递归基线条件
         if (prologueArr.length === 0 || middleOrderArr.length === 0) {
             return null;
         }
-        // 根结点
+        // 根结点元素
         const root = prologueArr[0];
+        // 根据根结点元素构建树节点
         const tree = new Node(root);
         // 获取根结点在中序遍历中的位置
         let index = 0;
@@ -44,13 +44,16 @@ export class TreeOperate<T> {
             index++;
         }
 
-        // 对于中序遍历其左边的节点就是左子树，其右边的节点就是右子树
         // 递归填充它的左子树
-        // 当前节点的前序遍历结果为前序遍历的1号位置到index位置(包含index)的元素
-        // 当前节点的中序遍历结果为中序遍历的0号位置index位置
+        // 在前序遍历中，根节点后面的index个元素及时它的左子树，剩余的就是它的右子树
+        // 在中序遍历中，根结点左边的节点就是左子树，剩余的就是它的右子树
+        // 因此，当前节点的前序遍历结果为前序遍历的1号位置到index位置(包含index)的元素
+        // 因此，当前节点的中序遍历结果为中序遍历的0号位置index位置
         tree.left = <Node<T>>this.buildBinaryTree(prologueArr.slice(1, index + 1), middleOrderArr.slice(0, index));
         // 递归填充它的右子树，左子树已经填充完成剩余的就是右子树，index+1到它的末尾
         tree.right = <Node<T>>this.buildBinaryTree(prologueArr.slice(index + 1), middleOrderArr.slice(index + 1));
+        // 返回tree，出栈，直至栈内元素被清空，二叉树重建完毕，问题解决。
         return tree;
     }
 }
+TreeOperate;
